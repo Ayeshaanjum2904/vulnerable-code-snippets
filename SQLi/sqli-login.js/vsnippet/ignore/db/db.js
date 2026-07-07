46bdf59f72524168fa1bb31e42e21f4b9b1fb88c
Ayeshaanjum2904 <ayeshaanjum2904@gmail.com>
2026-07-07 16:21:41 +0530

    security(secrets): automatically remediate 1 infrastructure issue(s) in db.js

--- SQLi/sqli-login.js/vsnippet/ignore/db/db.js
+++ SQLi/sqli-login.js/vsnippet/ignore/db/db.js
@@ -6,8 +6,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'db-mysql',
   database: 'ywhvsnippet',
-  user: 'vsnippet',
-  password: 'vsnippet',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
